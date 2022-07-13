b3e4924e4a45b9ed6a600b944137b6cee8320e0a
runner runner <jonsol7@protonmail.com>
2022-07-13 21:57:09 +0700

    t

--- components/NutritionFacts.jsx
+++ components/NutritionFacts.jsx
@@ -3,23 +3,23 @@ import React from 'react';
 const NutritionFacts = ({data}) => {
     console.log(data,"this")
   return (
-    <section className="performance-facts border bg-white shadow-lg w-full mr-5 p-5 rounded-lg">
-      <header className="performance-facts__header m-0 pb-1">
-        <h1 className="performance-facts__title text-3xl leading-none m-0 p-0">Nutrition Facts</h1>
-        <p className="leading-extratight text-sm">Serving Size 1 Burger</p>
+    <section classNameName="performance-facts border bg-white shadow-lg w-full mr-5 p-5 rounded-lg">
+      <header classNameName="performance-facts__header m-0 pb-1">
+        <h1 classNameName="performance-facts__title text-3xl leading-none m-0 p-0">Nutrition Facts</h1>
+        <p classNameName="leading-extratight text-sm">Serving Size 1 Burger</p>
       </header>
-      <table class="performance-facts__table">
+      <table className="performance-facts__table">
         <thead>
           <tr>
-            <th colspan="3" class="small-info text-xs">
+            <th colSpan="3" className="small-info text-xs">
               Amount Per Serving
             </th>
           </tr>
         </thead>
         <tbody>
             <tr>
-              <th colspan="2">
-                <span className="font-black">Calories </span>
+              <th colSpan="2">
+                <span classNameName="font-black">Calories </span>
                 {data.calories}
               </th>
               <td>
@@ -27,26 +27,26 @@ const NutritionFacts = ({data}) => {
                 111
               </td>
             </tr>
-            <tr class="thick-row">
-              <td colspan="3" class="small-info text-xs">
+            <tr className="thick-row">
+              <td colSpan="3" className="small-info text-xs">
                 % Daily Value*
               </td>
             </tr>
       </tbody>
     </table>
-      <table class="performance-facts__table">
+      <table className="performance-facts__table">
           <tbody>
             <tr>
-              <th colspan="2">
-              <span className="font-black">Total Fat </span>
+              <th colSpan="2">
+              <span classNameName="font-black">Total Fat </span>
               {data.total_fat}g
               </th>
               <td>
-              <span className="font-black">22%</span>
+              <span classNameName="font-black">22%</span>
               </td>
             </tr>
             <tr>
-              <td class="blank-cell">
+              <td className="blank-cell">
               </td>
               <th>
                 <span>Saturated Fat </span>
@@ -57,7 +57,7 @@ const NutritionFacts = ({data}) => {
               </td>
             </tr>
             <tr>
-              <td class="blank-cell">
+              <td className="blank-cell">
               </td>
               <th>
                 <span>Trans Fat </span>
@@ -67,34 +67,34 @@ const NutritionFacts = ({data}) => {
               </td>
             </tr>
             <tr>
-              <th colspan="2">
-              <span className="font-black">Cholesterol </span>
+              <th colSpan="2">
+              <span classNameName="font-black">Cholesterol </span>
               {data.cholesterol}mg
             </th>
             <td>
-            <span className="font-black">18%</span>
+            <span classNameName="font-black">18%</span>
               </td>
             </tr>
             <tr>
-              <th colspan="2">
-                <span className="font-black">Sodium </span>
+              <th colSpan="2">
+                <span classNameName="font-black">Sodium </span>
                 {data.sodium}mg
               </th>
               <td>
-              <span className="font-black">2%</span>
+              <span classNameName="font-black">2%</span>
               </td>
             </tr>
             <tr>
-              <th colspan="2">
-              <span className="font-black">Total Carbs </span>
+              <th colSpan="2">
+              <span classNameName="font-black">Total Carbs </span>
                 {data.total_carbohydrates}g
               </th>
               <td>
-              <span className="font-black">6%</span>
+              <span classNameName="font-black">6%</span>
               </td>
             </tr>
             <tr>
-              <td class="blank-cell">
+              <td className="blank-cell">
               </td>
               <th>
                 <span>Dietary Fiber </span>
@@ -105,7 +105,7 @@ const NutritionFacts = ({data}) => {
               </td>
             </tr>
             <tr>
-              <td class="blank-cell">
+              <td className="blank-cell">
               </td>
               <th>
                 <span>Sugars </span>
@@ -114,9 +114,9 @@ const NutritionFacts = ({data}) => {
               <td>
               </td>
             </tr>
-            <tr class="thick-end">
-              <th colspan="2">
-              <span className="font-black">Protein </span>
+            <tr className="thick-end">
+              <th colSpan="2">
+              <span classNameName="font-black">Protein </span>
               {data.protein}g
             </th>
               <td>
@@ -125,26 +125,26 @@ const NutritionFacts = ({data}) => {
           </tbody>
         </table>
 
-        <table class="performance-facts__table performance-facts__table--dots">
+        <table className="performance-facts__table performance-facts__table--dots">
           <tbody>
             <tr>
-              <td className="w-1/3">
+              <td classNameName="w-1/3">
                 Vitamin A
                 {data.vitamin_a}%
               </td>
-              <td className="text-center w-1/3">•</td>
-              <td className="w-1/3">
+              <td classNameName="text-center w-1/3">•</td>
+              <td classNameName="w-1/3">
                 Vitamin C
                 {data.vitamin_c}%
               </td>
             </tr>
-            <tr class="thin-end">
-              <td colspan="">
+            <tr className="thin-end">
+              <td colSpan="">
                 Calcium
                 {data.calcium}%
               </td>
-              <td className="text-center">•</td>
-              <td className="">
+              <td classNameName="text-center">•</td>
+              <td classNameName="">
                 Iron
                 {data.iron}%
               </td>
@@ -152,12 +152,12 @@ const NutritionFacts = ({data}) => {
           </tbody>
         </table>
 
-        <p class="text-xs leading-extratight pt-1">* Percent Daily Values are based on a 2,000 calorie diet. Your daily values may be higher or lower depending on your calorie needs:</p>
+        <p className="text-xs leading-extratight pt-1">* Percent Daily Values are based on a 2,000 calorie diet. Your daily values may be higher or lower depending on your calorie needs:</p>
 {/* 
-        <table class="performance-facts__table--small small-info">
+        <table className="performance-facts__table--small small-info">
           <thead>
             <tr>
-              <td colspan="2"></td>
+              <td colSpan="2"></td>
               <th>Calories:</th>
               <th>2,000</th>
               <th>2,500</th>
@@ -165,47 +165,47 @@ const NutritionFacts = ({data}) => {
           </thead>
           <tbody>
             <tr>
-              <th colspan="2">Total Fat</th>
+              <th colSpan="2">Total Fat</th>
               <td>Less than</td>
               <td>65g</td>
               <td>80g</td>
             </tr>
             <tr>
-              <td class="blank-cell"></td>
+              <td className="blank-cell"></td>
               <th>Saturated Fat</th>
               <td>Less than</td>
               <td>20g</td>
               <td>25g</td>
             </tr>
             <tr>
-              <th colspan="2">Cholesterol</th>
+              <th colSpan="2">Cholesterol</th>
               <td>Less than</td>
               <td>300mg</td>
               <td>300 mg</td>
             </tr>
             <tr>
-              <th colspan="2">Sodium</th>
+              <th colSpan="2">Sodium</th>
               <td>Less than</td>
               <td>2,400mg</td>
               <td>2,400mg</td>
             </tr>
             <tr>
-              <th colspan="3">Total Carbohydrate</th>
+              <th colSpan="3">Total Carbohydrate</th>
               <td>300g</td>
               <td>375g</td>
             </tr>
             <tr>
-              <td class="blank-cell"></td>
-              <th colspan="2">Dietary Fiber</th>
+              <td className="blank-cell"></td>
+              <th colSpan="2">Dietary Fiber</th>
               <td>25g</td>
               <td>30g</td>
             </tr>
           </tbody>
         </table>
-        <p class="small-info">
+        <p className="small-info">
           Calories per gram:
   </p>
-        <p class="small-info text-center">
+        <p className="small-info text-center">
           Fat 9
           &bull;
           Carbohydrate 4
