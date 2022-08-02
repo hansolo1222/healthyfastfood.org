4df5e1909448f4633c6912c921061a06f73ccb76
runner runner <jonsol7@protonmail.com>
2022-08-02 20:55:52 +0700

    speed test

--- components/FAQ.jsx
+++ components/FAQ.jsx
@@ -6,13 +6,13 @@ export const FAQ = ({faqs}) => {
             <div className="">
   
               <h2 className="text-3xl font-extrabold text-zinc-900">Frequently asked questions</h2>
-              <div className="border p-4">
+              {/* <div className="border p-4">
                 <ul>
                 {faqs.map((faq, index)=>(
                   <li><a href={`/#${index}`}>{faq.question}</a></li>
                 ))}
                 </ul>
-              </div>
+              </div> */}
             </div>
             <div className="mt-12 lg:mt-0 lg:col-span-2 faq">
               <dl className="space-y-12">
