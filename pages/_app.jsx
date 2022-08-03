deffa9d6cc83444af792607b6cec4cf07443f3f0
runner runner <jonsol7@protonmail.com>
2022-08-03 15:44:27 +0700

    t

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -9,7 +9,6 @@ function MyApp({ Component, pageProps }) {
   <>
     <Head>
       <link rel="shortcut icon" href="/images/favicon.ico" />
-    
       </Head>
 
       <script
@@ -26,20 +25,6 @@ function MyApp({ Component, pageProps }) {
             `,
           }}
         />
-{/* 
-      <Script id="gtag" strategy="lazyOnload" src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`} />
-
-<Script id="ga" strategy="lazyOnload">
-    {`
-        window.dataLayer = window.dataLayer || [];
-        function gtag(){dataLayer.push(arguments);}
-        gtag('js', new Date());
-        gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}', {
-        page_path: window.location.pathname,
-        });
-    `}
-</Script> */}
-
       <Component {...pageProps} />
   </>
   )
