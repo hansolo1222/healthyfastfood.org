2a4257efd8ac5fb5293e61e407dc2421043e7097
runner runner <jonsol7@protonmail.com>
2022-08-03 15:24:41 +0700

    tet

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -2,24 +2,28 @@ import '../styles/globals.css'
 import 'tailwindcss/tailwind.css'
 import Head from 'next/head'
 import Script from 'next/script'
+
+
 function MyApp({ Component, pageProps }) {
   return (
   <>
     <Head>
       <link rel="shortcut icon" href="/images/favicon.ico" />
-       <Script strategy="lazyOnload" src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`} />
-
-            <Script strategy="lazyOnload">
-                {`
-                    window.dataLayer = window.dataLayer || [];
-                    function gtag(){dataLayer.push(arguments);}
-                    gtag('js', new Date());
-                    gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}', {
-                    page_path: window.location.pathname,
-                    });
-                `}
-            </Script>
+    
       </Head>
+      <Script id="gtag" strategy="lazyOnload" src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`} />
+
+<Script id="ga" strategy="lazyOnload">
+    {`
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+        gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}', {
+        page_path: window.location.pathname,
+        });
+    `}
+</Script>
+
       <Component {...pageProps} />
   </>
   )
