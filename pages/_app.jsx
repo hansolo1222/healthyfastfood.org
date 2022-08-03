d1826cb5682279e2f45e430e0257a49e7881a582
runner runner <jonsol7@protonmail.com>
2022-08-03 15:32:20 +0700

    test

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -22,7 +22,7 @@ function MyApp({ Component, pageProps }) {
               window.dataLayer = window.dataLayer || [];
               function gtag(){dataLayer.push(arguments);}
               gtag('js', new Date());
-              gtag('config', ${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}, { page_path: window.location.pathname });
+              gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}', { page_path: window.location.pathname });
             `,
           }}
         />
