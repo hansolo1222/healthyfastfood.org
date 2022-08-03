af03cc4272d578be846de7fe555dd5868fe4a27b
runner runner <jonsol7@protonmail.com>
2022-08-03 15:10:04 +0700

    ganalytics

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,11 +1,24 @@
 import '../styles/globals.css'
 import 'tailwindcss/tailwind.css'
 import Head from 'next/head'
+import Script from 'next/script'
 function MyApp({ Component, pageProps }) {
   return (
   <>
     <Head>
       <link rel="shortcut icon" href="/images/favicon.ico" />
+       <Script strategy="lazyOnload" src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`} />
+
+            <Script strategy="lazyOnload">
+                {`
+                    window.dataLayer = window.dataLayer || [];
+                    function gtag(){dataLayer.push(arguments);}
+                    gtag('js', new Date());
+                    gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}', {
+                    page_path: window.location.pathname,
+                    });
+                `}
+            </Script>
       </Head>
       <Component {...pageProps} />
   </>
