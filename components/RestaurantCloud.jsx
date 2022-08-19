a218cc4dc6b7df8314a7eccd6f0c6f6202a386f3
runner runner <jonsol7@protonmail.com>
2022-08-19 22:02:37 +0700

    mobile home page formatting

--- components/RestaurantCloud.jsx
+++ components/RestaurantCloud.jsx
@@ -3,14 +3,14 @@ import Link from 'next/link';
 export default function RestaurantCloud({restaurants}) {
   return (
     <div className="bg-white">
-    <h2 className="text-3xl font-bold text-center mt-12">Most Popular Restaurants</h2>
-      <div className="max-w-7xl mx-auto py-12 px-4 sm:px-6 lg:px-8">
-        <div className="grid grid-cols-2 md:grid-cols-6 lg:grid-cols-5" >
+    <h2 className="text-xl md:text-3xl font-bold text-center mt-6 md:mt-12">Most Popular Restaurants</h2>
+      <div className="max-w-7xl mx-auto py-6 md:py-12 px-4 sm:px-6 lg:px-8">
+        <div className="grid grid-cols-3 md:grid-cols-6 lg:grid-cols-5" >
           {restaurants.map((restaurant)=>(
             <Link href={`/${restaurant.slug}`} key={restaurant.slug}>
             <div className="hover:bg-stone-100 rounded-xl py-4">
               <div className="col-span-1 flex items-center justify-center md:col-span-2 lg:col-span-1 cursor-pointer">
-                <img className="h-16 w-auto" src={`/images/logos_large/${restaurant.slug}.webp`} alt={`${restaurant.name} Logo`} />
+                <img className="h-12 md:h-16 w-auto" src={`/images/logos_large/${restaurant.slug}.webp`} alt={`${restaurant.name} Logo`} />
               </div>
               </div>
             </Link>
