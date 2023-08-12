e12d18bd1bcb8ce055d1c205cb27f5e38a2f47f4
runner runner <jonsol7@protonmail.com>
2023-08-12 14:13:16 +0700

    r

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -2,7 +2,7 @@ import '../styles/globals.css'
 import 'tailwindcss/tailwind.css'
 import Head from 'next/head'
 import Script from 'next/script'
-const GTM_ID = 'G-PSTY3EB8KQ';
+const GTMID = 'G-PSTY3EB8KQ';
 
 
 function MyApp({ Component, pageProps }) {
@@ -36,7 +36,7 @@ function MyApp({ Component, pageProps }) {
     new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
     j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
     'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-    })(window,document,'script','dataLayer', ${GTM_ID});
+    })(window,document,'script','dataLayer', ${GTMID});
   `,
   }}
 />
