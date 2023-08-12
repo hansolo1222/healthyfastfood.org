0d404f988f2ce2d4412b38de7672c0140bdbdc69
runner runner <jonsol7@protonmail.com>
2023-08-12 14:12:09 +0700

    r

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -27,23 +27,19 @@ function MyApp({ Component, pageProps }) {
     <Head>
       <link rel="shortcut icon" href="/images/favicon.ico" />
       {/* <!-- Google Tag Manager --> */}
-            <script
-              type="text/javascript"
-              async
-              dangerouslySetInnerHTML={{
-                __html: `
-              (function(w, d, s, l, i) {
-                    w[l] = w[l] || []
-                    w[l].push({ "gtm.start": new Date().getTime(), event: "gtm.js" })
-                    const f = d.getElementsByTagName(s)[0],
-                      j = d.createElement(s),
-                      dl = l != "dataLayer" ? "&l=" + l : ""
-                    j.async = true
-                    j.src = "https://www.googletagmanager.com/gtm.js?id=" + i + dl
-                    f.parentNode.insertBefore(j, f)
-                  })(window,document,'script','dataLayer',"<GTM ID>")`,
-              }}
-            />
+      <Script
+  strategy="afterInteractive"
+  id="your-id"
+  dangerouslySetInnerHTML={{
+    __html: `
+    (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+    new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+    j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+    'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+    })(window,document,'script','dataLayer', ${GTM-ID});
+  `,
+  }}
+/>
 {/* <!-- End Google Tag Manager --> */}
         </Head>
 
