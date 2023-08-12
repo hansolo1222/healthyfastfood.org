22594fa87d77c3974d6004928c500121f6a43490
runner runner <jonsol7@protonmail.com>
2023-08-12 13:53:35 +0700

    add GTM script

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -6,21 +6,23 @@ import { useEffect } from 'react'
 import Script from 'next/script'
 import { useRouter } from 'next/router'
 import * as gtag from '../lib/gtag'
+const GTM_ID = 'G-PSTY3EB8KQ';
+
 
 function MyApp({ Component, pageProps }) {
-  const router = useRouter()
-  useEffect(()=>{
-    const handleRouteChange = (url) => {
-      gtag.pageview(url)
-    }
-    router.events.on('routeChangeComplete', handleRouteChange)
-    router.events.on('hashChangeComplete', handleRouteChange)
-    return () => {
-      router.events.off('routeChangeComplete', handleRouteChange)
-      router.events.off('hashChangeComplete', handleRouteChange)
+  // const router = useRouter()
+  // useEffect(()=>{
+  //   const handleRouteChange = (url) => {
+  //     gtag.pageview(url)
+  //   }
+  //   router.events.on('routeChangeComplete', handleRouteChange)
+  //   router.events.on('hashChangeComplete', handleRouteChange)
+  //   return () => {
+  //     router.events.off('routeChangeComplete', handleRouteChange)
+  //     router.events.off('hashChangeComplete', handleRouteChange)
 
-    }
-  }, [router.events])
+  //   }
+  // }, [router.events])
 
 
 
@@ -45,7 +47,15 @@ function MyApp({ Component, pageProps }) {
             `,
           }}
         /> */}
-
+        <Script id="google-tag-manager" strategy="afterInteractive">
+        {`
+        (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+        new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+        j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+        'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+        })(window,document,'script','dataLayer','${GTM_ID}');
+        `}
+    </Script>
         </Head>
 
       <Component {...pageProps} />
