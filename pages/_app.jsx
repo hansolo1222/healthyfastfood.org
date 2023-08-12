3b8653542716048081faa2cfe1ea80954d579bf5
runner runner <jonsol7@protonmail.com>
2023-08-12 14:01:15 +0700

    GTM with script element

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,11 +1,7 @@
 import '../styles/globals.css'
 import 'tailwindcss/tailwind.css'
 import Head from 'next/head'
-
-import { useEffect } from 'react'
 import Script from 'next/script'
-import { useRouter } from 'next/router'
-import * as gtag from '../lib/gtag'
 const GTM_ID = 'G-PSTY3EB8KQ';
 
 
@@ -30,32 +26,25 @@ function MyApp({ Component, pageProps }) {
   <>
     <Head>
       <link rel="shortcut icon" href="/images/favicon.ico" />
-      {/* <script defer src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-4191723359468026"
-     crossOrigin="anonymous"></script> */}
-      
-      {/* <script
-          async
-          src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
-        />
-        <script
-          dangerouslySetInnerHTML={{
-            __html: `
-              window.dataLayer = window.dataLayer || [];
-              function gtag(){dataLayer.push(arguments);}
-              gtag('js', new Date());
-              gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}', { page_path: window.location.pathname });
-            `,
-          }}
-        /> */}
-        <Script id="google-tag-manager" strategy="afterInteractive">
-        {`
-        (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-        new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-        j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-        'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-        })(window,document,'script','dataLayer','${GTM_ID}');
-        `}
-    </Script>
+      {/* <!-- Google Tag Manager --> */}
+            <script
+              type="text/javascript"
+              async
+              dangerouslySetInnerHTML={{
+                __html: `
+              (function(w, d, s, l, i) {
+                    w[l] = w[l] || []
+                    w[l].push({ "gtm.start": new Date().getTime(), event: "gtm.js" })
+                    const f = d.getElementsByTagName(s)[0],
+                      j = d.createElement(s),
+                      dl = l != "dataLayer" ? "&l=" + l : ""
+                    j.async = true
+                    j.src = "https://www.googletagmanager.com/gtm.js?id=" + i + dl
+                    f.parentNode.insertBefore(j, f)
+                  })(window,document,'script','dataLayer',"<GTM ID>")`,
+              }}
+            />
+{/* <!-- End Google Tag Manager --> */}
         </Head>
 
       <Component {...pageProps} />
