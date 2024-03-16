7292811941b5b7a22b5e96e72dc977cd1f0f508e
jonsol <jonsol7@protonmail.com>
2024-03-16 18:24:44 +0800

    r

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
   reactStrictMode: true,
-  swcMinify: true,
+  swcMinify: false,
   i18n: {
     locales: ["en-US", "de", "uk", "fr", "it", "es", "ca", "au"],
     defaultLocale: "en-US",
