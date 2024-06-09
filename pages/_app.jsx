76fa11d97c58e7ccaf9fda44f32127ad93b10714
jonsol <jonsol7@protonmail.com>
2024-06-09 16:37:29 +0700

    test commit

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,30 +1,20 @@
 import '../styles/globals.css'
 import 'tailwindcss/tailwind.css'
 import Head from 'next/head'
-import Script from 'next/script'
-const GTM_ID = 'G-PSTY3EB8KQ';
+ const GTM_ID = 'G-PSTY3EB8KQ';
+import { GoogleAnalytics } from '@next/third-parties/google'
 
 
 function MyApp({ Component, pageProps }) {
   return (
   <>
-   <Script
-  strategy="afterInteractive"
-  id="your-id"
-  dangerouslySetInnerHTML={{
-    __html: `
-    (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-    new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-    j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-    'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-    })(window,document,'script','dataLayer', '${GTM_ID}');
-  `,
-  }}
-/>
+    
     <Head>
       <link rel="shortcut icon" href="/images/favicon.ico" />
     </Head>
     <Component {...pageProps} />
+    <GoogleAnalytics gaId="G-PSTY3EB8KQ" />
+
   </>
   )
 }
